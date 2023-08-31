2420d03a6a6b9c8c8c978ef9075f19a6b88cf111
Tomasz <pawlowski.t.93@gmail.com>
2023-08-31 21:33:49 +0200

    Task 1 and 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+gallery.style.listStyle = `none`;
+
+galleryItems.forEach(picture => {
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<li><a class="gallery__item" href="${picture.original}">
+  <img class="gallery__image" src="${picture.preview}" alt="${picture.description}" />
+</a></li>`
+  );
+});
+for (let i = 0; i < galleryItems.length; i++) {
+  let link = gallery.childNodes[i].firstChild;
+  link.style.height = '100%';
+  link.style.display = 'block';
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
