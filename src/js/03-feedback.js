3f23dcc4018dce9aae6bb38ef36ad8f1c7ce1909
Tomasz <pawlowski.t.93@gmail.com>
2023-09-01 18:54:28 +0200

    All tasks done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const inputObj = {};
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+try {
+  const savedFeedbackState = localStorage.getItem('feedback-form-state');
+  const parsedFeedbackState = JSON.parse(savedFeedbackState);
+  feedbackForm.email.value = parsedFeedbackState.email;
+  feedbackForm.message.value = parsedFeedbackState.message;
+} catch (error) {
+  console.log("Inputs haven't been filled yet.");
+}
+
+const saveInLocalStorage = event => {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  inputObj.email = email.value;
+  inputObj.message = message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputObj));
+};
+
+const submitEvent = event => {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(inputObj);
+};
+
+feedbackForm.addEventListener('input', throttle(saveInLocalStorage), 500);
+feedbackForm.addEventListener('submit', submitEvent);
